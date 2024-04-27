a5d00243468534b7e062fd40b3660ee4dd86a70b
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-04-27 18:08:22 +0200

    Update Main_Alarma.java

--- Ejercicios_Profe_Pedro/Main_Alarma.java
+++ Ejercicios_Profe_Pedro/Main_Alarma.java
@@ -5,13 +5,15 @@ public class Main_Alarma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Alarma alarma1 = new Alarma (11, 11);
+		Alarma alarma1 = new Alarma (11, 51);
 		Alarma.atributo_hora = true;
 		alarma1.activarAlarma();
 		System.out.println(alarma1 + " " + alarma1.infoAlarma());
 		
 		Alarma.atributo_hora = false;
-		alarma1.avanzarTiempo(44);
+		alarma1.avanzarTiempo(59);
+		alarma1.avanzarTiempo(11);
+		alarma1.retrocederTiempo(5);
 		alarma1.desactivarAlarma();
 		System.out.println(alarma1 + " " + alarma1.infoAlarma());
 	}
