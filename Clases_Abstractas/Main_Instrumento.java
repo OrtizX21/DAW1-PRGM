65235ec1dcc7b607dcef845654dd0952d6a362e6
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-05-02 15:35:37 +0200

    Update Main_Instrumento.java

--- Clases_Abstractas/Main_Instrumento.java
+++ Clases_Abstractas/Main_Instrumento.java
@@ -1,5 +1,4 @@
 package Clases_Abstractas;
-
 import Clases_Abstractas.Instrumento.notaMusical;
 
 public class Main_Instrumento {
@@ -7,14 +6,18 @@ public class Main_Instrumento {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// CREAMOS LAS NOTAS MUSICALES DEL PIANO.
 		notaMusical musica[] = {notaMusical.DO, notaMusical.FA, notaMusical.RE, notaMusical.SOL};
 		
+		// CREAMOS EL OBJETO PIANO QUE HEREDA LOS METODOS DE INSTRUMENTO.
 		Piano piano1 = new Piano();
 		
+		// MIENTRAS QUE EL CONTADOR SEA MENOR A LA LONGITUD DE LA TABLA USAREMOS EL METODO ADD DE INSTRUMENTO PARA INSERTAR LAS NOTAS MUSICALES.
 		for(int i = 0; i < musica.length; i++) {
 			piano1.add(musica[i]);
 		}
 		
+		// MOSTRAMOS COMO SE TOCA EL PIANO.
 		piano1.interpretar();
 	}
 
