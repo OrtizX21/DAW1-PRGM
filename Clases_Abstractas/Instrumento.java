a01ccc4811182d6c1e33a6c3d4465d60d573fa22
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-05-02 15:34:56 +0200

    Update Instrumento.java

--- Clases_Abstractas/Instrumento.java
+++ Clases_Abstractas/Instrumento.java
@@ -1,8 +1,11 @@
 package Clases_Abstractas;
 import java.util.*;
 
+// DEFINIMOS UNA CLASE ABSTRACTA LO CUAL ES UNA CLASE QUE ESTA EN DESARROLLO Y LA VAMOS A UTILIZAR PARA CREAR OBJETOS CON LO QUE 
+// HAY DENTRO DE ESTA CLASE Y DESARROLLAREMOS EL METODO INTERPRETAR DEPENDIENDO DE LAS NOTAS DE LOS INSTRUMENTOS (CLASES QUE CREAREMOS Y HEREDAREMOS).
 public abstract class Instrumento {
 	
+	//ARRAY DE TIPO NOTA MUSICAL, EL NOMBRE DE ESTE ES (MELODIA).
 	public enum notaMusical {DO, RE, MI, FA, SOL, LA, SI};
 	protected notaMusical[] melodia;
 	
@@ -15,5 +18,6 @@ public abstract class Instrumento {
 		melodia[melodia.length - 1] = nota;
 	}
 	
+	// METOOD ABSTRACTO QUE DESARROLLAREMOS EN CAMPANA Y PIANO PARA SABER COMO SE TOCA LA PARTITURA.
 	abstract void interpretar();
 }
