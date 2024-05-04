20231c7e20299d2912f2bb5d5689caccb2e29469
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-05-04 15:37:07 +0200

    Update Main_Gestion_Contactos.java

--- Ejercicios_Profe_Pedro/Main_Gestion_Contactos.java
+++ Ejercicios_Profe_Pedro/Main_Gestion_Contactos.java
@@ -5,6 +5,7 @@ public class Main_Gestion_Contactos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//CREANDO EL CONTACTO Y AÑADIENDO LOS CONTACTOS.
 		Gestion_Contactos contacto1 = new Gestion_Contactos(4);
 		
 		Contacto novia = new Contacto("Milena", "Sanchez", "115180", "Milenita@sa11");
@@ -16,10 +17,13 @@ public class Main_Gestion_Contactos {
 		contacto1.agregarContacto(bro2);
 		System.out.println(contacto1 + "\n");
 		
+		//ELIMINANDO CONTACTO.
 		contacto1.eliminarContacto(0);
-		System.out.println("Despues de eliminar un contacto: " + contacto1);
-
-	}
-
-	
+		System.out.println("Despues de eliminar un contacto: " + contacto1 + "\n");
+		
+		//ACTUALIZANDO CONTACTO Y BUSCANDO POR NOMBRE Y APELLIDO
+		contacto1.actualizarContacto(novia, 0);
+		contacto1.buscarContactoPorNombre("Jose");
+		contacto1.buscarContactoPorApellido("Sanchez");
+	}	
 }
