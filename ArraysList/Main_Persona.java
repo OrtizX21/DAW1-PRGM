4457dd1790ff818e878660c8ca0a60edab45c628
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-05-21 19:26:35 +0200

    Update Main_Persona.java

--- ArraysList/Main_Persona.java
+++ ArraysList/Main_Persona.java
@@ -19,7 +19,16 @@ public class Main_Persona {
 	System.out.println();
 	Collections.sort(lista);
 	
+	Persona p1 = new Persona(18, 84, 173);
+	lista.add(4, p1);
+	p1.setNif("AX445532");
+	Persona p2 = lista.get(4);
+	
+	System.out.println("Esta en la lista? " + lista.contains(p1) + ", Posicion : " + lista.indexOf(p1) + ", Posicion final: " + lista.lastIndexOf(p1) + "\n" );
+	System.out.println("Esta en la lista? " + lista.contains(p2) + ", Posicion : " + lista.indexOf(p2) + ", Posicion final: " + lista.lastIndexOf(p2) + "\n" );
+	
 	System.out.println("Despues de ordenar la lista con Collections: " + "\n");
+	
 	for(Persona collections : lista) {
 		System.out.println(collections);
 		}
